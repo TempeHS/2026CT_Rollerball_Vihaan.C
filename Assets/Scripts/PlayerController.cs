86d02ae533426f35a20429d26438454bd46ddedb
ItzVihaan <vihaan.chattaraj@education.nsw.gov.au>
2026-05-14 09:13:28 +1000

    fixed player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  private Rigidbody rb; 
-private float movementX;
-private float movementY;
-  
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>(); 
+ // Rigidbody of the player.
+ private Rigidbody rb; 
 
+ // Movement along X and Y axes.
+ private float movementX;
+ private float movementY;
+
+ // Speed at which the player moves.
+ public float speed = 0; 
+
+ // Start is called before the first frame update.
+ void Start()
+    {
+ // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
+    }
+ 
+ // This function is called when a move input is detected.
+ void OnMove(InputValue movementValue)
+    {
+ // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+ // Store the X and Y components of the movement.
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
     }
 
- void OnMove(InputValue movementValue)
-   {
-     Vector2 movementVector = movementValue.Get<Vector2>(); 
+ // FixedUpdate is called once per fixed frame-rate frame.
+ void FixedUpdate() 
+    {
+ // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
-   movementX = movementVector.x; 
-   movementY = movementVector.y; 
-   }
-
-    void FixedUpdate() 
-   {
-    Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-   rb.AddForce(movementVector); 
-   }
+ // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed); 
+    }
 }
